f9c6f02d32245cd1d079aa03c54acde2ff346ef1
Tonystar05 <yedhuku2005@gmail.com>
2024-10-21 21:04:14 +0530

    Update config.py
    Gg

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from logging.handlers import RotatingFileHandler
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "15529802"))
-API_HASH = os.environ.get("API_HASH", "92bcb6aa798a6f1feadbc917fccb54d3")
+API_ID = int(os.environ.get("API_ID", "27050683"))
+API_HASH = os.environ.get("API_HASH", "013a5c0b1f2c320b98236cf212835d59")
 
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "821215952"))
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://PREMIUM:PREMIUM@cluster0.fteks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "PREMIUM")
+OWNER_ID = int(os.environ.get("OWNER_ID", "6048003536"))
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://2gbRenameAUG:2gbRenameAUG@cluster0.uaysx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "FileStoreXeon2Bot")
 
 START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
 
@@ -20,7 +20,7 @@ START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Priv
 START_PIC = os.environ.get("START_PIC", "https://envs.sh/SPw.jpg")
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/SPq.jpg")
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002427224406"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002325565780"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002329702484"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001542373688"))
 
@@ -73,7 +73,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need 
 
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(821215952)
+ADMINS.append(6848088376)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
